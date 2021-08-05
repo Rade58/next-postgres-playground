cca30af1601584abb8b65b409b4a284552f79a4f
Rade <bajic.rade2@gmail.com>
2021-08-05 13:44:27 +0200

    🦉

--- db/SQL/tables/users.sql
+++ db/SQL/tables/users.sql
@@ -1,8 +1,6 @@
 CREATE TABLE users(
   user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   nano_id VARCHAR (48),
-  -- random_id ,
-
   email VARCHAR (50) UNIQUE NOT NULL,
   nickname VARCHAR (100) NOT NULL,
   last_login TIMESTAMP,
