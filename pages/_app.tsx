d6ea2d13c08f045d5f52713fe9a4b8a1dee975de
Rade58 <bajic.rade2@gmail.com>
2021-08-07 19:17:51 +0200

    🦉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,47 @@
-import "../styles/globals.css"
-import {useState, useEffect} from 'react'
-import Link from 'next/link'
-import type { AppProps } from 'next/app'
-import type {User} from '@supabase/supabase-js'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import type { AppProps } from "next/app";
+import type { User } from "@supabase/supabase-js";
 
-
-import {supabase} from '../lib/supabase'
-
+import { supabase } from "../lib/supabase";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const [user, setUser] = useState<null| User>(null)
+  const [user, setUser] = useState<null | User>(null);
 
   useEffect(() => {
     // CHECKING FOR USER WHEN SOMETHING CHANGES WITH AUTH
-    const {data: authListener} = supabase.auth.onAuthStateChange(async (e) => checkUser() )
-    
+    const { data: authListener } = supabase.auth.onAuthStateChange(async (e) =>
+      checkUser()
+    );
+
     // CHECKING USER ON MOUNTING
-    checkUser()
+    checkUser();
 
     return () => {
       // NO NEED TO CHECK FOR USER WHEN COMMPONENT UNMOUNTS
-      authListener?.unsubscribe()
-    }
-  }, [])
+      authListener?.unsubscribe();
+    };
+  }, []);
 
-  async function checkUser(){
-    const user = supabase.auth.user()
+  async function checkUser() {
+    const user = supabase.auth.user();
 
-    setUser(user)
+    setUser(user);
   }
 
-  return <div>
+  return (
+    <div>
+      <nav className="p-6 border-b border-gray-300">
+        <Link href="/">
+          <a className="mr-6 cursor-pointer">Home</a>
+        </Link>
+      </nav>
 
-    <div className="py-8 px-16">
-
-    <Component {...pageProps} />
+      <div className="py-8 px-16">
+        <Component {...pageProps} />
+      </div>
     </div>
-  </div> 
-  
-  
+  );
 }
-export default MyApp
+export default MyApp;
