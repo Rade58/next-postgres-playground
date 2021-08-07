5911f254b642a4a5027eb61541f8170244685360
Rade58 <bajic.rade2@gmail.com>
2021-08-07 15:51:56 +0200

    🦉

--- db/SQL/SUPABASE/posts_table.sql
+++ db/SQL/SUPABASE/posts_table.sql
@@ -14,10 +14,12 @@ CREATE TABLE posts(
 
 ALTER TABLE posts ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY 'Individuals can create posts.' ON posts FOR INSERT WITH CHECK (auth.uid() = user_id);
+-- PUT DOUBLE QUOTES HERE
 
-CREATE POLICY 'Individuals can update their own posts.' ON posts FOR UPDATE USING (auth.uid() = user_id);
+CREATE POLICY "Individuals can create posts." ON posts FOR INSERT WITH CHECK (auth.uid() = user_id);
 
-CREATE POLICY 'Individuals can delete their own posts.' ON posts FOR DELETE USING (auth.uid() = user_id);
+CREATE POLICY "Individuals can update their own posts." ON posts FOR UPDATE USING (auth.uid() = user_id);
 
-CREATE POLICY 'Post are public.' ON posts FOR SELECT USING (true);
+CREATE POLICY "Individuals can delete their own posts." ON posts FOR DELETE USING (auth.uid() = user_id);
+
+CREATE POLICY "Post are public." ON posts FOR SELECT USING (true);
