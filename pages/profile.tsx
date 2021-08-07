9591a12f4cb02f5fa59943dcde4650f03edce291
Rade58 <bajic.rade2@gmail.com>
2021-08-07 20:12:00 +0200

    🦉

---
+++ pages/profile.tsx
@@ -0,0 +1,40 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import { FunctionComponent } from "react";
+
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+import { Auth, Typography, Button } from "@supabase/ui";
+import { supabase } from "../lib/supabase";
+
+const { Text } = Typography;
+
+const Profile: FunctionComponent<{
+  supabaseClient: SupabaseClient;
+}> = ({ supabaseClient, children }) => {
+  const { user } = Auth.useUser();
+
+  if (user) {
+    return (
+      <>
+        <Text>Signeed in: {user?.email}</Text>
+        <Button>Sign out</Button>
+      </>
+    );
+  }
+
+  // I RETURNED null HERE
+  return null;
+
+  // TUTORIAL MAKER RETURNED CHILDREN AND I DON'T KNOW WHY
+  // TYPESCRIPT IS YELLING WHEN YOU DO THIS BECAUSE
+  // THIS IS NOT FC ANYMORE IS YOU DO THAT
+
+  // return children;
+};
+
+const ProfilePage: FunctionComponent = () => {
+  return <div></div>;
+};
+
+export default ProfilePage;
