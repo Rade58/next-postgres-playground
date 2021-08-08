17cd2c43bb09237620ac389b3997e21b11dbad2a
Rade58 <bajic.rade2@gmail.com>
2021-08-08 21:18:53 +0200

    🦉

--- pages/edit-post/[id].tsx
+++ pages/edit-post/[id].tsx
@@ -1,6 +1,15 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import type { FunctionComponent } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import { supabase } from "../../lib/supabase";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const EditPostPage: FunctionComponent = () => {
   return <div></div>;
