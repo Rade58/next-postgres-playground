c323c8112c1f58655c6d7d6b1defe03b387b3e28
Rade58 <bajic.rade2@gmail.com>
2021-08-08 21:01:32 +0200

    🦉

---
+++ pages/edit-post/[id].tsx
@@ -0,0 +1,9 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { FunctionComponent } from "react";
+
+const EditPostPage: FunctionComponent = () => {
+  return <div></div>;
+};
+
+export default EditPostPage;
