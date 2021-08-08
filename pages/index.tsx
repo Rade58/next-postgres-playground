e338e76793fdcb8fbc46c5d13f5d332ee523ad13
Rade58 <bajic.rade2@gmail.com>
2021-08-08 16:59:08 +0200

    🦉

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
+import type { FC } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
-import type { FC} from 'react'
+import { supabase } from "../lib/supabase";
 
-
-const IndexPage:FC = () => {
-
-  return (
-    <div className="bg-gray-600 text-gray-100">Hello World</div>
-  )
-
+interface DataI {
+  id: string;
+  title: string;
+  user_email: string;
 }
+
+const IndexPage: FC = () => {
+  const [posts, setPosts] = useState<DataI[]>([]);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const { data, error } = await supabase.from("posts").select();
+
+    setPosts(data as DataI[]);
+    setIsLoading(false);
+  }
+
+  return <div className="bg-gray-600 text-gray-100">Hello World</div>;
+};
 
 export default IndexPage;
