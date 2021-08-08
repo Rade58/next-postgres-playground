ad70fffc265d0e035daf7dcca4c5ff881f8f87d7
Rade58 <bajic.rade2@gmail.com>
2021-08-08 16:41:20 +0200

    🦉

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SINGLE POST PAGE, A PAGE WITH A DYNAMIC ROUTE
 
-WE ARE GOING TO USE getStaticProps AND getStaticPaths
+WE ARE GOING TO USE getStaticProps AND getStaticPaths (I'M GOING TO USE revalidate (COURSE AUTHOR DIDN'T USE revalidate))
 
 ```
 touch pages/posts/[id].tsx
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,17 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import { FunctionComponent } from "react";
-import { GetStaticPaths, GetStaticProps } from "next";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
 import { supabase } from "../../lib/supabase";
 
-interface FeedbackPagePropsI {
-  post: any;
+interface postsPagePropsI {
+  post: {
+    title: string;
+    user_email: string;
+    content: string;
+  } | null;
 }
 
 // id SINCE FILLE IS CALLED [id]
@@ -43,7 +48,7 @@ export const getStaticPaths: GetStaticPaths<paramsType> = async (ctx) => {
   };
 };
 
-export const getStaticProps: GetStaticProps<FeedbackPagePropsI, paramsType> =
+export const getStaticProps: GetStaticProps<postsPagePropsI, paramsType> =
   async (ctx) => {
     const { params } = ctx;
 
@@ -62,7 +67,8 @@ export const getStaticProps: GetStaticProps<FeedbackPagePropsI, paramsType> =
       // THIS IS A WAY TO WRITE     WHERE    CLAUSE IN
       // (SINCE THIS WILL BE TRNSLATED INTO SQL)
       // WE ARE QUERYING BY ID
-      .filter("id", "eq", params.id);
+      .filter("id", "eq", params.id)
+      .single();
 
     if (!data) {
       return {
@@ -80,18 +86,32 @@ export const getStaticProps: GetStaticProps<FeedbackPagePropsI, paramsType> =
     };
   };
 
-const FeedbackPage: FunctionComponent<FeedbackPagePropsI> = ({ post }) => {
+const PostPage: FunctionComponent<postsPagePropsI> = ({ post }) => {
   console.log({ post });
 
-  const { query } = useRouter();
+  const { query } = useRouter(); // slug IS AVAILABLE AS query.id
 
-  return <div>Page ID: {query.id}</div>;
+  return (
+    <>
+      {post && (
+        <div>
+          <h1 className="text-5xl mt-4 font-semibold tracking-wide">
+            {post.title}
+          </h1>
+          <p className="text-sm font-light my-4">by {post.user_email}</p>
+          <div>
+            <ReactMarkdown className="prose">{post.content}</ReactMarkdown>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default FeedbackPage;
+export default PostPage;
 
-// We use getStaticProps to fetch all the feedback for the site,
-//  given the siteId forwarded by the dynamic route.
+// We use getStaticProps to fetch all the posts for the site,
+//  given the id forwarded by the dynamic route.
 //  We forward that information to the component via a prop.
 
 // We use getStaticPaths to create a page for each site.
@@ -101,7 +121,7 @@ export default FeedbackPage;
 //  This is controlled by the fallback value of true.
 
 // We define a revalidation period of one second with revalidate.
-//  Every feedback page is built statically at build time.
+//  Every posts page is built statically at build time.
 //  Then, when a request comes in after the revalidation period,
 //  getStaticProps is re-ran behind the scenes.
 //  If it completes successfully, the page is replaced
