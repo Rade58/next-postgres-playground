2dc38511b4296f1a6c78021e19a931edb568a8ad
Rade58 <bajic.rade2@gmail.com>
2021-08-10 22:55:43 +0200

    🦉

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,33 +3,51 @@
 import { FunctionComponent } from "react";
 import type { GetServerSideProps } from "next";
 
+// TYPES FOR OUR DATA ARE ALSO AVAILABLE
+// WE JUST NED Post FOR NOW
+// BUT I AM SHOWING YOU THAT YOU HAVE ALL TYPES FOR DATA
+import type { Post, User } from "@prisma/client";
+
 // WE WILL USE PRISMA CLIENT
 import prismaClient from "../../lib/prisma";
 
 interface PropsI {
-  placeholder: boolean;
+  allPosts: (Post & {
+    author: {
+      id: number;
+      name: string | null;
+    } | null;
+  })[];
 }
 
 export const getServerSideProps: GetServerSideProps<PropsI> = async (ctx) => {
   // WE WILL QUERY FOR ALL POSTS IN HERE
 
-  const {} = await prismaClient.post.findMany({
+  const allPosts = await prismaClient.post.findMany({
     where: { published: true },
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
   });
 
   return {
     props: {
-      placeholder: true,
+      allPosts,
     },
   };
 };
 
 const IndexBlogPage: FunctionComponent<PropsI> = (props) => {
-  //
+  // LETS JUST DISPLAY STRING FOR NOW
 
-  console.log(props);
+  const { allPosts } = props;
 
-  return <div>🦉</div>;
+  return <div>{JSON.stringify({ allPosts })}</div>;
 };
 
 export default IndexBlogPage;
