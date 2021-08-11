61fd225c63b81be014592bf061eeeb5093f15ba9
Rade58 <bajic.rade2@gmail.com>
2021-08-11 15:01:55 +0200

    🦉

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import React from "react";
+import type { FC } from "react";
+
+import { useRouter } from "next/router";
+
+import { signOut, useSession } from "next-auth/client";
+
+const Header: FC = () => {
+  return <></>;
+};
+
+export default Header;
