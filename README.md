ad198fe12d911725130e453f619b6eca53269081
Rade58 <bajic.rade2@gmail.com>
2021-08-12 20:26:04 +0200

    🦉

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FUNCTIONALITY FOR ADDING NEW BLOG POSTS
 
+```
+touch components/Layout.tsx
+```
+
+```tsx
+
+```
+
+
+
+```
+touch pages/blog/create.tsx
+```
+
+```tsx
+
+```
---
+++ pages/blog/create.tsx
@@ -0,0 +1,9 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import { FunctionComponent } from "react";
+
+const CreateBlogPost: FunctionComponent = () => {
+  return <div>👾</div>;
+};
+
+export default CreateBlogPost;
