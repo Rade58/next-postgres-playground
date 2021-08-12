888ab9d0e93df41559e332a5db21bdec709c0f03
Rade58 <bajic.rade2@gmail.com>
2021-08-12 20:47:20 +0200

    🦉

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,8 +4,8 @@
 import { jsx, css } from "@emotion/react";
 import type { FC } from "react";
 
-const Lyout: FC = () => {
-  return null;
+const Layout: FC = ({ children }) => {
+  return <main className="border-2 border-gray-600">{children}</main>;
 };
 
-export default Lyout;
+export default Layout;
--- pages/blog/create.tsx
+++ pages/blog/create.tsx
@@ -1,9 +1,45 @@
-/* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
-import { FunctionComponent } from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import { useState } from "react";
+import type { FunctionComponent, SyntheticEvent } from "react";
+
+import Router from "next/router";
+
+import Layout from "../../components/Layout";
 
 const CreateBlogPost: FunctionComponent = () => {
-  return <div>👾</div>;
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const submitData = async (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Layout>
+      <div>
+        <form onScroll={submitData}></form>
+        <h1>New Draft</h1>
+        <input
+          type="text"
+          name="Title"
+          placeholder="Title"
+          autoFocus
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          name="Content"
+          placeholder="Content"
+          cols={50}
+          rows={8}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+      </div>
+    </Layout>
+  );
 };
 
 export default CreateBlogPost;
