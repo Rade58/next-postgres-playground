52449426f83ed80c2f295b23cb58014a58769f95
Rade58 <bajic.rade2@gmail.com>
2021-08-12 22:53:15 +0200

    🦉

---
+++ pages/blog/drafts.tsx
@@ -0,0 +1,13 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+import type { FunctionComponent } from "react";
+
+const DraftsPage: FunctionComponent = () => {
+  return <div>👾</div>;
+};
+
+export default DraftsPage;
