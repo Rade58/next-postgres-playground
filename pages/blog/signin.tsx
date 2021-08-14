2b338d5b00ac29bdc5cb80b014572331b9b36dd4
Rade58 <bajic.rade2@gmail.com>
2021-08-14 14:53:41 +0200

    🦉

--- pages/blog/signin.tsx
+++ pages/blog/signin.tsx
@@ -1,11 +1,23 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
-import { FunctionComponent } from "react";
+import { useState, useEffect } from "react";
+import type { FunctionComponent } from "react";
+
+import Router from "next/router";
+
+import { signIn, getSession } from "next-auth/client";
 
 const SigninPage: FunctionComponent = () => {
+  useEffect(() => {
+    getSession().then((ses) => {
+      if (ses) Router.push("/blog/drafts");
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className="border-2 border-gray-900 max-w-md mx-auto mt-11">
+      <button className="border-2 border-gray-500">Sign In With Github</button>
+      <button className="border-2 border-gray-500">Sign In With Google</button>
     </div>
   );
 };
